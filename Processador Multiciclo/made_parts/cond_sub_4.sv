d2b48de5b442afea275673efc3a7378262420935
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2023-07-18 14:23:37 -0300

    Create cond_sub_4.sv

---
+++ Processador Multiciclo/made_parts/cond_sub_4.sv
@@ -0,0 +1,11 @@
+// cond sub 4
+// corrijam se tiver errado
+module cond_sub_4(
+  input wire Break,
+  input wire[31:0] PC,
+  output wire[31:0] saida
+);
+
+  assign saida = (Break ? PC - 32'd4 : PC);
+  
+endmodule
