3856ac4cd5875210b6e0d152ece7a6b667d0840e
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2023-07-18 14:04:25 -0300

    Create sign-extend.sv

---
+++ Processador Multiciclo/made_parts/sign-extend.sv
@@ -0,0 +1,16 @@
+// sign extend 16-32
+module signextend(
+input reg[15:0] entrada, // instruction 15:0
+output reg[31:0] saida
+);
+
+always@(*)begin
+  if (entrada[15]) begin
+      saida = {16'd65535,entrada}; // 2^16-1 concatenado  
+  end
+  else begin
+    saida = {16'b0, entrada};
+  end
+end
+
+endmodule
