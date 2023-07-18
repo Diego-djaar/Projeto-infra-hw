a8cef6a054746e56e743d4f8f136ac57744aa326
João Vítor Régis <jvcr@cin.ufpe.br>
2023-07-18 19:37:30 -0300

    Dei continuidade a LU, falta implementar a ULA aux

--- Processador Multiciclo/made_parts/logic_unit.v
+++ Processador Multiciclo/made_parts/logic_unit.v
@@ -1,27 +1,33 @@
 module logic_unit(
     input wire clk,
     input wire reset,
-    input wire [32:0] ALUSrcA,
-    input wire [32:0] ALUSrcB,
+    input wire [31:0] ALUSrcA,
+    input wire [31:0] ALUSrcB,
     input wire [4:0] SHAMT,
     input wire [3:0] ALUOp,
     output wire SPECIAL,
     output wire OVERFLOW,
     output wire ZERO,
-    output wire [32:0] ALUOut,
+    output wire [31:0] ALUOut,
     output wire Update_UC
 );
     // Control wires
     wire [2:0] ALU_control;
+    wire [2:0] SHIFTER_control;
+    wire M_SHIFTER;
+    wire [2:0] M_ALUOut_control;
 
     // Data wires
-    wire [32:0] ALU_out;
+    wire [31:0] ALU_out;
     wire Of;
     wire Ng;
     wire Zr;
     wire Eq;
     wire Gt;
     wire Lt;
+    wire [31:0] SHIFTER_out;
+    wire [4:0] M_SHIFTER_out;
+    wire [31:0] EXTEND_out;
 
 
     // Criar um encoder pra traduzir ALUOp nas operações da ULA/SHIFTER
@@ -39,8 +45,33 @@ module logic_unit(
         Lt
     );
 
-    RegDesloc SHIFTER_(
+    mux_shifter mux_shifter_(
+        M_SHIFTER,
+        SHAMT,
+        M_SHIFTER_out
+    );
 
+    RegDesloc SHIFTER_(
+        clk,
+        reset,
+        SHIFTER_control,
+        M_SHIFTER_out,
+        ALUSrcB,
+        SHIFTER_out
+    );
+
+    extender_1_to_32 extender_1_to_32_(
+        Lt,
+        EXTEND_out
+    );
+
+    mux_ALUOut mux_ALUOut_(
+        M_ALUOut_control,
+        ALU_aux, // ainda n implementada
+        ALU_out,
+        SHIFTER_out,
+        ALUSrcA,
+        EXTEND_out
     );
 
 
---
+++ Processador Multiciclo/made_parts/ulaaux.v
@@ -0,0 +1,10 @@
+module ulaaux (
+    input wire [31:0] A,
+    input wire [31:0] B,
+    input wire [1:0] ulaaux_control, // Operações a ser definidas
+    output wire ulaux_out,
+    output wire SPECIAL
+);
+
+
+endmodule
