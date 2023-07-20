f139bef41b2fe21c149522106eb7eb5fa8ed5930
João Vítor Régis <jvcr@cin.ufpe.br>
2023-07-20 15:11:24 -0300

    Update UC implementado

--- Processador Multiciclo/made_parts/update_UC.v
+++ Processador Multiciclo/made_parts/update_UC.v
@@ -4,18 +4,21 @@ module update_UC(
     input wire maior,
     input wire menor,
     input wire UC_control, // Definir se vai enviar o valor pra UC ou não
-    input wire [1:0] UC_op // Selecionar entre BEQ, BNE, BLE E BGT
+    input wire [1:0] UC_op, // Selecionar entre BEQ, BNE, BLE E BGT
     output wire update_UC_out
 );
 
-    always @(*)
-    begin
-        case (UC_op)
-            2'b00:
-            2'b01:
-            2'b10:
-            2'b11:
-        endcase
-    end
+        always @(*)
+        begin
+            update_UC_out = 1'b0; // Não sei se essa linha é necessária
+        if (UC_control == 1'b1) begin
+            case (UC_op)
+                2'b00: update_UC_out = (igual) ? 1'b1 : 1'b0; // BEQ
+                2'b01: update_UC_out = (igual) ? 1'b0 : 1'b1; // BNE
+                2'b10: update_UC_out = (maior) ? 1'b0 : 1'b1; // BLE
+                2'b11: update_UC_out = (maior) ? 1'b1 : 1'b0; // BGT
+            endcase
+        end
+        end
 
 endmodule
