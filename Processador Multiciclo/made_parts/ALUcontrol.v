da778f1c48f6e3d95acb98af51a5b7f48c730e2e
João Vítor Régis <jvcr@cin.ufpe.br>
2023-07-21 14:04:57 -0300

    ALUcontrol quase finalizada, só falta a ALUaux

--- Processador Multiciclo/made_parts/ALUcontrol.v
+++ Processador Multiciclo/made_parts/ALUcontrol.v
@@ -5,18 +5,18 @@ module ALUcontrol(
     output wire [2:0] ALU_control;
     output wire [2:0] SHIFTER_control;
     output wire M_SHIFTER;
-    output wire [2:0] M_ALUOut_control;
+    output wire [1:0] M_ALUOut_control;
     output wire UC_control;
     output wire [1:0] UC_op;
 );
     // Como tudo aqui vai ser executado em um ciclo não é necessário usar um contador
 
     // Operações
-    parameter NO_OP = 4'b0000;
+    parameter NO_OP = 4'b0000; // equivalente a PASS_A
     parameter ADD = 4'b0001;
     parameter SUB = 4'b0010;
     parameter AND = 4'b0011;
-    parameter PASS_A = 4'b0100;
+    parameter PASS_B  = 4'b0100;
     parameter SHIFT_L1 = 4'b0101;
     parameter SHIFT_L2 = 4'b0110;
     parameter SHIFT_R = 4'b0111;
@@ -34,7 +34,7 @@ module ALUcontrol(
             ALU_control = 3'b000;
             SHIFTER_control = 3'b000;
             M_SHIFTER = 1'b0;
-            M_ALUOut_control = 3'b000;
+            M_ALUOut_control = 3'b00;
             UC_control = 1'b0;
             UC_op = 2'b00;
         end
@@ -43,10 +43,115 @@ module ALUcontrol(
                 ALU_control = 3'b000;
                 SHIFTER_control = 3'b000;
                 M_SHIFTER = 1'b0;
-                M_ALUOut_control = 3'b000;
+                M_ALUOut_control = 2'b01;
                 UC_control = 1'b0;
                 UC_op = 2'b00;
-            end
+            end else if (ALUOp == ADD) begin
+                ALU_control = 3'b001;
+                SHIFTER_control = 3'b000;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b01;
+                UC_control = 1'b0;
+                UC_op = 2'b00;
+            end else if (ALUOp == SUB) begin
+                ALU_control = 3'b010;
+                SHIFTER_control = 3'b000;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b01;
+                UC_control = 1'b0;
+                UC_op = 2'b00;
+            end else if (ALUOp == AND) begin
+                ALU_control = 3'b011;
+                SHIFTER_control = 3'b000;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b01;
+                UC_control = 1'b0;
+                UC_op = 2'b00;
+            end else if (ALUOp == PASS_B) begin
+                ALU_control = 3'b000;
+                SHIFTER_control = 3'b000;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b10;
+                UC_control = 1'b0;
+                UC_op = 2'b00;
+            end else if (ALUOp == SHIFT_L1) begin
+                ALU_control = 3'b000;
+                SHIFTER_control = 3'b010;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b10;
+                UC_control = 1'b0;
+                UC_op = 2'b00;
+            end else if (ALUOp == SHIFT_L2) begin // ALUaux
+                ALU_control = 3'b000;
+                SHIFTER_control = 3'b010;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b10;
+                UC_control = 1'b0;
+                UC_op = 2'b00;
+            end else if (ALUOp == SHIFT_R) begin
+                ALU_control = 3'b000;
+                SHIFTER_control = 3'b011;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b10;
+                UC_control = 1'b0;
+                UC_op = 2'b00;
+            end else if (ALUOp == SHIFT_RA1) begin
+                ALU_control = 3'b000;
+                SHIFTER_control = 3'b100;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b10;
+                UC_control = 1'b0;
+                UC_op = 2'b00;
+            end else if (ALUOp == SHIFT_RA2) begin // ALUaux
+                ALU_control = 3'b000;
+                SHIFTER_control = 3'b010;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b10;
+                UC_control = 1'b0;
+                UC_op = 2'b00;
+            end else if (ALUOp == SLTI) begin
+                ALU_control = 3'b111;
+                SHIFTER_control = 3'b000;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b11;
+                UC_control = 1'b0;
+                UC_op = 2'b00;
+            end else if (ALUOp == BEQ) begin
+                ALU_control = 3'b111;
+                SHIFTER_control = 3'b000;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b11;
+                UC_control = 1'b1;
+                UC_op = 2'b00;
+            end else if (ALUOp == BNE) begin
+                ALU_control = 3'b111;
+                SHIFTER_control = 3'b000;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b11;
+                UC_control = 1'b1;
+                UC_op = 2'b01;
+            end else if (ALUOp == BLE) begin
+                ALU_control = 3'b111;
+                SHIFTER_control = 3'b000;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b11;
+                UC_control = 1'b1;
+                UC_op = 2'b10;
+            end else if (ALUOp == BGT) begin
+                ALU_control = 3'b111;
+                SHIFTER_control = 3'b000;
+                M_SHIFTER = 1'b0;
+                M_ALUOut_control = 2'b11;
+                UC_control = 1'b1;
+                UC_op = 2'b11;
+            end else if (ALUOp == LUI) begin
+                ALU_control = 3'b000;
+                SHIFTER_control = 3'b010; // Shift left
+                M_SHIFTER = 1'b1;
+                M_ALUOut_control = 2'b10;
+                UC_control = 1'b0;
+                UC_op = 2'b00;
+            end 
         end
     end
 
--- Processador Multiciclo/made_parts/mux_ALUOut.v
+++ Processador Multiciclo/made_parts/mux_ALUOut.v
@@ -1,21 +1,19 @@
 module mux_ALUOut(
-    input wire [2:0] selector,
+    input wire [1:0] selector,
     input wire [31:0] Data_0, // ALUresult AUX
     input wire [31:0] Data_1, // ALUresult
     input wire [31:0] Data_2, // SHIFTER
-    input wire [31:0] Data_3, // ALUSourceA
-    input wire [31:0] Data_4, // 1-32 bit extender
+    input wire [31:0] Data_3, // 1-32 bit extender
     output wire [31:0] Data_out // ALUOut
 );
 
     always @(*)
     begin
         case (selector)
-            3'b000: Data_out = Data_0; // ALUresult AUX
-            3'b001: Data_out = Data_1; // ALUresult
-            3'b010: Data_out = Data_2; // SHIFTER
-            3'b011: Data_out = Data_3; // ALUSourceA
-            3'b100: Data_out = Data_4; // 1-32 bit extender
+            2'b00: Data_out = Data_0; // ALUresult AUX
+            2'b01: Data_out = Data_1; // ALUresult
+            2'b10: Data_out = Data_2; // SHIFTER
+            2'b11: Data_out = Data_3; // 1-32 bit extender
             default: Data_out = 32'bxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx; // Default
         endcase
     end
