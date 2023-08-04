2cc283d39a75a26208d5fac2218cf22b3ec0eee4
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2023-08-04 14:09:03 -0300

    Update div.sv

--- Processador Multiciclo/made_parts/div.sv
+++ Processador Multiciclo/made_parts/div.sv
@@ -1,100 +1,98 @@
 module div(
-    input wire reset,
-    input wire aluop, // presumindo sinal de um bit, ajeitar dps
-    input wire clk,
-  input wire [31:0] x, 
-  input wire [31:0] y,
-    output reg fim,
-    output reg div_zero,
-  output reg [31:0] lo,
-  output reg [31:0] hi
+input wire [31:0] a,
+input wire [31:0] b,
+input wire clk,
+input wire reset,
+input wire div_control,
+output reg [31:0] hi,
+output reg [31:0] lo,
+output reg operando
 );
 
-reg [63:0] b; // y
-reg [63:0] quo;
+reg [31:0] A;
+reg [31:0] B;
 reg signed [63:0] rem;
-integer n;
-reg operando;
+reg [63:0] quo;
 reg neg;
+  
+reg [5:0] n;
 
 parameter lim = {31{1'b1}};
+
 initial n = 32;
+initial hi = 32'b0;
+initial lo = 32'b0;
+initial operando = 1'b0;
+initial A = 32'b0;
+initial B = 32'b0;
+initial rem = 64'b0;
+initial quo = 64'b0;
+initial neg = 1'b0;
 
-
-
-always @(posedge clk) 
+  
+always @(posedge clk)
 begin
-    if (aluop && !reset)
-    begin 
-        operando = 1'b1;
-    end
-    if (reset) 
-    begin 
-      	div_zero = 1'b0;
-        n = 32;
-      	neg = 1'b0;
-        b = 64'b0;
-        quo = 64'b0;
-        rem = 64'b0;
-        operando = 1'b0;
-    end
-  if (operando && !div_zero) // tá certo botar verificação de div 0 aqui?
+  if (!operando && div_control)
     begin
-        if (n == 32) // começo
-        begin
-          rem = {{32{1'b0}},x};
-          b = {{32{1'b0}},y};
-          if (b == 0)
-          begin
-            // tem que interromper ou só sinalizar?
-            div_zero = 1'b1;
-          end
-            neg = 1'b0;
-          if (x >= lim)
-            begin
-                rem = ~rem+1; // operar como se fosse positivo
-                neg = ~neg;
-            end
-          if (y >= lim)
-            begin
-                b = ~b+1; // operar como positivo
-                neg = ~neg;
-            end
-            quo = 64'b0;
-        end
-        else if (n == 0) // fim
-        begin
-            // lógica sinalizada
-            if (neg)
-            begin
-                quo = ~quo+1;
-            end
-            hi = quo[63:32];
-            lo = quo[31:0];
-            fim = 1'b1;
-            operando = 1'b0;
-        end
-        else // operação
-        begin 
-            rem = $signed(rem - b);
-            if (rem < 0)
-            begin
-                rem = rem + b;
-                quo = quo << 1;
-                quo[0] = 1'b0;
-            end
-            else
-            begin
-                quo = quo << 1;
-                quo[0]=1'b1;
-            end
-            b = b >> 1;
-        end
-        n = n-1;
-    end
+		operando = 1'b1;
+		n = 32;
+	end
 
+	if (reset) 
+    begin
+		n = 32;
+		hi = 32'b0;
+		n = 32;
+		hi = 32'b0;
+		lo = 32'b0;
+		operando = 1'b0;
+		A = 32'b0;
+		B = 32'b0;
+		rem = 64'b0;
+		quo = 64'b0;
+		neg = 1'b0;
+	end
+	
+	if (operando) 
+    begin	
+		if(n == 32) //comeco
+        begin
+			neg = 1'b0;
+			if (a >= lim) neg = ~neg;
+			if (b >= lim) neg = ~neg;
+			A = a;
+			rem = {{32'b0},A};
+			B = b;
+ 		end 
+		else if(n == 0) // fim
+        begin
+			if (neg)
+			begin
+				quo = ~quo+1;
+			end
+			hi = quo[63:32];
+			lo = quo[31:0];
+			operando = 1'b0;
+		end		 
+		else 
+        begin
+			// operacao de fato
+			rem = rem - $signed(B);
+			if (rem >= 0)
+			begin
+				quo = quo << 1;
+				quo[0] = 1'b1;
+			end
+			else
+			begin
+				rem = rem + B;
+				quo = quo << 1;
+				quo[0] = 1'b0;
+			end
+			B = B >> 1;
+		end
+		n = n-1;
+    end	
 end
 
-
-    
 endmodule
