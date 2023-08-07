0763efafee80764a458829f9412cdeb597eab497
Diego <djaar@cin.ufpe.br>
2023-08-07 17:44:50 -0300

    Compile Fix: adicionar case ausente em STR_JR e typo no PC_w

--- Processador Multiciclo/made_parts/control_unit.v
+++ Processador Multiciclo/made_parts/control_unit.v
@@ -399,6 +399,7 @@ module control_unit (
                     endcase
                 end
                 STR_JR: begin
+                  case (COUNTER)
                         0: begin
                             A_reg_w = WRITE;
                             COUNTER = COUNTER + 1;
@@ -411,7 +412,7 @@ module control_unit (
                         2: begin
                             A_reg_w = READ;
                             Mux_PC = 2'b00;
-                            PC_W = 1'b1;
+                            PC_w = 1'b1;
                             COUNTER = COUNTER + 1;
                         end
                         3: begin // Esperar escrever no PC
@@ -422,6 +423,7 @@ module control_unit (
                             PC_w = 1'b0;
                             STATE = ST_PC_MAIS_4;
                         end
+                  endcase
                 end
             endcase
         end
