1cfb6c68ac391fe31efa8efec77915e0379ef04b
Diego <djaar@cin.ufpe.br>
2023-08-07 23:21:55 -0300

    Bugfix: SRAV funcionando corretamente com números negativos

--- Processador Multiciclo/made_parts/ulaaux.v
+++ Processador Multiciclo/made_parts/ulaaux.v
@@ -8,6 +8,6 @@ module ulaaux (
     wire [31:0] A1;
     
     assign A1 = (ulaaux_control[0]) ? A << B : B; // Faz um shift left se for 10
-    assign ulaaux_out = (ulaaux_control[1]) ? A >>> B : A1;  // e um shift right aritmetico se for 01
+    assign ulaaux_out = (ulaaux_control[1]) ? (A >> B) | (-($unsigned(A) >> 31) << (32 - B)) : A1;  // e um shift right aritmetico se for 01
 
 endmodule
