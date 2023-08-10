a6e90d96fab460714c99cc089fc94180668ab43f
Diego <djaar@cin.ufpe.br>
2023-08-10 00:21:31 -0300

    Completar mux5to1

--- Processador Multiciclo/made_parts/mux5to1.v
+++ Processador Multiciclo/made_parts/mux5to1.v
@@ -1,5 +1,5 @@
 module mux5to1 (
-    input wire [1:0] selector,
+    input wire [2:0] selector,
     input wire [31:0] Data_0,
     input wire [31:0] Data_1,
     input wire [31:0] Data_2,
@@ -10,11 +10,11 @@ module mux5to1 (
     always @(*)
     begin
         case (selector)
-            2'b00: Data_out = Data_0;
-            2'b01: Data_out = Data_1;
-            2'b10: Data_out = Data_2;
-            2'b11: Data_out = Data_3;
-            // Faltando Data_4. Necessário aumentar o tamanho do seletor.
+            3'b000: Data_out = Data_0;
+            3'b001: Data_out = Data_1;
+            3'b010: Data_out = Data_2;
+            3'b011: Data_out = Data_3;
+            3'b100: Data_out = Data_4;
             default: Data_out = 32'bxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx; // Default
         endcase
     end
