2d6db52fd10dd759aac17724802dbf78ee0b26e2
João Vítor Régis <jvcr@cin.ufpe.br>
2023-08-12 16:02:55 -0300

    Sign extend bug fix

--- Processador Multiciclo/made_parts/signextend.sv
+++ Processador Multiciclo/made_parts/signextend.sv
@@ -4,13 +4,6 @@ input wire [15:0] entrada, // instruction 15:0
 output reg [31:0] saida
 );
 
-always@(*)begin
-  if (entrada[15]) begin
-      saida = {16'd65535,entrada}; // 2^16-1 concatenado  
-  end
-  else begin
-    saida = {16'b0, entrada};
-  end
-end
+assign saida = (entrada[15]) ? {16'b1111111111111111, entrada} : {16'b0, entrada};
 
 endmodule
